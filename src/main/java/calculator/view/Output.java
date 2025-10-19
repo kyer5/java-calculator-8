1ea9f7260819a35196084a53cfd22eef56184302
강예린 <yerin7786@gmail.com>
2025-10-19 20:09:24 +0900

    feat(Output): 출력 기능 구현
    출력 형식에 따라 계산 결과를 출력한다.

---
+++ src/main/java/calculator/view/Output.java
@@ -0,0 +1,10 @@
+package calculator.view;
+
+public class Output {
+
+    private static final String OUTPUT_MESSAGE = "결과 : ";
+
+    public void printCalculateResult(int sum) {
+        System.out.println(OUTPUT_MESSAGE + sum);
+    }
+}
