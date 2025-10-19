bf65d123bc318540b382ca196fc008f216ab542b
강예린 <yerin7786@gmail.com>
2025-10-20 02:44:41 +0900

    refactor(Application): 예외 처리 방식 개선
    스택트레이스로 표시되던 예외를 콘솔에 메시지로 출력되도록 변경한다.
    - try-catch 블록 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,6 +5,10 @@ import calculator.controller.CalculatorController;
 public class Application {
     public static void main(String[] args) {
         CalculatorController calculatorController = new CalculatorController();
-        calculatorController.run();
+        try {
+            calculatorController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
